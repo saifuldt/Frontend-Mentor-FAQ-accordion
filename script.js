4bc336d9f7a45c6ff24b9ad6335a89e6bf73ddaa
saifuldt <saifulislam897722@gmail.com>
2024-06-05 12:49:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const pluse1 = document.getElementById("pluse");
+const minus1 = document.getElementById("minus");
+const para1 = document.getElementById("para");
+
+pluse1.addEventListener('click', () => {
+
+    para1.style.display = 'block';
+    pluse1.style.visibility = 'hidden';
+    pluse1.style.display = 'none';
+    minus1.style.visibility = 'visible';
+    minus1.style.display = 'block';
+
+});
+
+minus1.addEventListener('click', () => {
+
+    para1.style.display = 'none';
+    minus1.style.visibility = 'hidden';
+    pluse1.style.visibility = 'visible';
+    minus1.style.display = 'none';
+    pluse1.style.display = 'block';
+
+});
+
+
+
+// ************2******
+
+const pluse2 = document.getElementById("pluse2");
+const minus2 = document.getElementById("minus2");
+const para2 = document.getElementById("para2");
+
+pluse2.addEventListener('click', () => {
+
+    para2.style.display = 'block';
+    pluse2.style.visibility = 'hidden';
+    pluse2.style.display = 'none';
+    minus2.style.visibility = 'visible';
+    minus2.style.display = 'block';
+
+});
+
+minus2.addEventListener('click', () => {
+
+    para2.style.display = 'none';
+    minus2.style.visibility = 'hidden';
+    pluse2.style.visibility = 'visible';
+    minus2.style.display = 'none';
+    pluse2.style.display = 'block';
+
+});
+
+// ***********3*********
+
+
+const pluse3 = document.getElementById("pluse3");
+const minus3 = document.getElementById("minus3");
+const para3 = document.getElementById("para3");
+
+pluse3.addEventListener('click', () => {
+
+    para3.style.display = 'block';
+    pluse3.style.visibility = 'hidden';
+    pluse3.style.display = 'none';
+    minus3.style.visibility = 'visible';
+    minus3.style.display = 'block';
+
+});
+
+minus3.addEventListener('click', () => {
+
+    para3.style.display = 'none';
+    minus3.style.visibility = 'hidden';
+    pluse3.style.visibility = 'visible';
+    minus3.style.display = 'none';
+    pluse3.style.display = 'block';
+
+});
+
+// **********4*******
+
+const pluse4 = document.getElementById("pluse4");
+const minus4 = document.getElementById("minus4");
+const para4 = document.getElementById("para4");
+
+pluse4.addEventListener('click', () => {
+
+    para4.style.display = 'block';
+    pluse4.style.visibility = 'hidden';
+    pluse4.style.display = 'none';
+    minus4.style.visibility = 'visible';
+    minus4.style.display = 'block';
+
+});
+
+minus4.addEventListener('click', () => {
+
+    para4.style.display = 'none';
+    minus4.style.visibility = 'hidden';
+    pluse4.style.visibility = 'visible';
+    minus4.style.display = 'none';
+    pluse4.style.display = 'block';
+
+});
+
+
